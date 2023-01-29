b48a396b7377c47def941c6fe5577720610b3a13
Pascual Cano <pascual@allhuman.com>
2023-01-29 18:55:43 +0100

    Case 5, introducing digits map

--- src/roman.ts
+++ src/roman.ts
@@ -1,3 +1,6 @@
+const digits = { 1: "I", 5: "V" };
+
 export const numberToRoman = (n: number): string => {
-	return "".padStart(n, "I");
+	if (n >= 5) return digits[5];
+	return "".padStart(n, digits[1]);
 };
--- tests/roman.spec.ts
+++ tests/roman.spec.ts
@@ -4,10 +4,16 @@ describe("Roman numbers tests", () => {
 	test(`return I for 1`, () => {
 		expect(numberToRoman(1)).toBe("I");
 	});
+
 	test(`return II for 2`, () => {
 		expect(numberToRoman(2)).toBe("II");
 	});
+
 	test(`return III for 3`, () => {
 		expect(numberToRoman(3)).toBe("III");
+	});
+
+	test(`return V for 5`, () => {
+		expect(numberToRoman(5)).toBe("V");
 	});
 });
