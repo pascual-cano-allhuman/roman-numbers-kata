c35f80d8bde016551ce242ba6281335b7fd03833
Pascual Cano <pascual@allhuman.com>
2023-01-29 18:16:25 +0100

    Case 3, using padStart

--- src/roman.ts
+++ src/roman.ts
@@ -1,3 +1,3 @@
 export const numberToRoman = (n: number): string => {
-  return n > 1 ? "II" : "I";
+	return "".padStart(n, "I");
 };
--- tests/roman.spec.ts
+++ tests/roman.spec.ts
@@ -1,10 +1,13 @@
 import { numberToRoman } from "../src/roman";
 
 describe("Roman numbers tests", () => {
-  test(`return I for 1`, () => {
-    expect(numberToRoman(1)).toBe("I");
-  });
-  test(`return II for 2`, () => {
-    expect(numberToRoman(2)).toBe("II");
-  });
+	test(`return I for 1`, () => {
+		expect(numberToRoman(1)).toBe("I");
+	});
+	test(`return II for 2`, () => {
+		expect(numberToRoman(2)).toBe("II");
+	});
+	test(`return III for 3`, () => {
+		expect(numberToRoman(3)).toBe("III");
+	});
 });
