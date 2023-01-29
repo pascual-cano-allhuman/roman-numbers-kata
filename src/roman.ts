0ab7616bf8fc08cbbd69f3e4cd6da6bd99cac502
Pascual Cano <pascual@allhuman.com>
2023-01-29 13:49:29 +0100

    First test

--- src/roman.ts
+++ src/roman.ts
@@ -1,3 +1,3 @@
 export const numberToRoman = (n: number): string => {
-  return "";
+  return "I";
 };
--- tests/roman.spec.ts
+++ tests/roman.spec.ts
@@ -1,5 +1,7 @@
+import { numberToRoman } from "../src/roman";
+
 describe("Roman numbers tests", () => {
-  test(`Dummy test`, () => {
-    expect(true).toBe(true);
+  test(`return I for 1`, () => {
+    expect(numberToRoman(1)).toBe("I");
   });
 });
